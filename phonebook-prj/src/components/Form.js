a78f596c1d893aca9eb72d939cb32ee814c76153
leemiran0729 <leemiran.0729@gmail.com>
2023-06-14 01:23:50 +0900

    style: Form 컴포넌트 스타일 적용

--- phonebook-prj/src/components/Form.js
+++ phonebook-prj/src/components/Form.js
@@ -28,13 +28,34 @@ const Form = ({ info, setInfo }) => {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <input placeholder="이름" value={name} onChange={handleChangeName} />
-      <input placeholder="전화번호" value={tel} onChange={handleChangeTel} />
-      <button type="submit" onSubmit={handleSubmit}>
-        등록
-      </button>
-    </form>
+    <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm shadow rounded p-10">
+      <img
+        className="m-auto"
+        src="https://cdn1.iconfinder.com/data/icons/unicons-line-vol-5/24/phone-volume-128.png"
+        alt="phonebook"
+      />
+      <form className="space-y-6" onSubmit={handleSubmit}>
+        <input
+          className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-400 sm:text-sm sm:leading-6"
+          placeholder="이름"
+          value={name}
+          onChange={handleChangeName}
+        />
+        <input
+          className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-400 sm:text-sm sm:leading-6"
+          placeholder="전화번호"
+          value={tel}
+          onChange={handleChangeTel}
+        />
+        <button
+          className="flex w-full justify-center rounded-md bg-indigo-400 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          type="submit"
+          onSubmit={handleSubmit}
+        >
+          등록
+        </button>
+      </form>
+    </div>
   );
 };
 
