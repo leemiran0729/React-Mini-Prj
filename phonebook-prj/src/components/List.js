50e3565b0f454985cb0a761a3c1a6e2e904566dd
leemiran0729 <leemiran.0729@gmail.com>
2023-06-14 11:54:31 +0900

    style: List컴포넌트 스타일 적용

--- phonebook-prj/src/components/List.js
+++ phonebook-prj/src/components/List.js
@@ -32,31 +32,51 @@ const List = ({ info, setInfo, data }) => {
 
   if (isEditing) {
     return (
-      <li>
-        <form onSubmit={() => handleEdit(data.id)}>
-          <input
-            placeholder={data.name}
-            value={editedName}
-            onChange={handleChangeName}
-          />
-          <input
-            placeholder={data.tel}
-            value={editedTel}
-            onChange={handleChangeTel}
-          />
-          <button type="submit" onSubmit={() => handleEdit(data.id)}>
-            적용
-          </button>
-        </form>
-      </li>
+      <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm shadow rounded p-10">
+        <li>
+          <form onSubmit={() => handleEdit(data.id)}>
+            <input
+              className="lock w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-400 sm:text-sm sm:leading-6"
+              placeholder={data.name}
+              value={editedName}
+              onChange={handleChangeName}
+            />
+            <input
+              className="lock w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-400 sm:text-sm sm:leading-6"
+              placeholder={data.tel}
+              value={editedTel}
+              onChange={handleChangeTel}
+            />
+            <button
+              className="mt-2 rounded p-1 bg-indigo-400 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 mx-2 w-20"
+              type="submit"
+              onSubmit={() => handleEdit(data.id)}
+            >
+              적용
+            </button>
+          </form>
+        </li>
+      </div>
     );
   } else {
     return (
-      <li>
-        이름: {data.name}, 전화번호: {data.tel}
-        <button onClick={() => setIsEditing(true)}>수정</button>
-        <button onClick={() => handleRemove(data.id)}>삭제</button>
-      </li>
+      <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm shadow rounded p-10">
+        <li>
+          <p>이름: {data.name}</p> <p>전화번호: {data.tel}</p>
+          <button
+            className="rounded p-1 bg-indigo-400 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 mx-2 w-20 mt-2"
+            onClick={() => setIsEditing(true)}
+          >
+            수정
+          </button>
+          <button
+            className="rounded p-1 bg-indigo-400 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 mx-2 w-20 mt-2"
+            onClick={() => handleRemove(data.id)}
+          >
+            삭제
+          </button>
+        </li>
+      </div>
     );
   }
 };
--- phonebook-prj/src/components/Lists.js
+++ phonebook-prj/src/components/Lists.js
@@ -3,7 +3,7 @@ import List from "./List";
 
 const Lists = ({ info, setInfo }) => {
   return (
-    <div>
+    <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm shadow rounded p-10">
       <ul>
         {info.map((data, index) => (
           <List key={index} info={info} setInfo={setInfo} data={data}></List>
