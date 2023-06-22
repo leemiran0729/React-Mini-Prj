37102bffc9b0e5d947a0720f3c73930f1d7fe620
leemiran0729 <leemiran.0729@gmail.com>
2023-06-22 15:27:35 +0900

    feat: Search 컴포넌트 UI 구현

--- user-search-prj/src/components/Search.js
+++ user-search-prj/src/components/Search.js
@@ -1,7 +1,35 @@
 import React from "react";
+import styled from "styled-components";
 
 const Search = () => {
-  return <div>Search</div>;
+  return (
+    <SearchContainer>
+      <Input type="text" />
+      <Button>검색</Button>
+    </SearchContainer>
+  );
 };
 
 export default Search;
+
+const SearchContainer = styled.div`
+  margin: 10px;
+`;
+
+const Input = styled.input`
+  border: 1px solid #04b486;
+  border-radius: 5px;
+  height: 40px;
+  margin-right: 10px;
+`;
+
+const Button = styled.button`
+  width: 70px;
+  height: 44px;
+  background-color: #04b486;
+  color: #fff;
+  border: none;
+  border-radius: 5px;
+  outline: none;
+  cursor: pointer;
+`;
