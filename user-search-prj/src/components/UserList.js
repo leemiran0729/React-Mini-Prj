90ff942463fa5759121550cec0806806410c2c18
leemiran0729 <leemiran.0729@gmail.com>
2023-06-22 15:49:31 +0900

    userList 컴포넌트 UI 구현

--- user-search-prj/src/components/UserList.js
+++ user-search-prj/src/components/UserList.js
@@ -1,7 +1,63 @@
 import React from "react";
+import styled from "styled-components";
 
 const UserList = ({ users }) => {
-  return <div></div>;
+  return (
+    <ListContainer>
+      <Table>
+        <Tr>
+          <Th>id</Th>
+          <Th>name</Th>
+          <Th>username</Th>
+          <Th>email</Th>
+          <Th>website</Th>
+          <Th>address</Th>
+        </Tr>
+        {users.map((user) => (
+          <Tr key={user.id}>
+            <Td>{user.id}</Td>
+            <Td>{user.name}</Td>
+            <Td>{user.username}</Td>
+            <Td>{user.email}</Td>
+            <Td>{user.website}</Td>
+            <Td>
+              {user.city +
+                " " +
+                user.district +
+                " " +
+                user.street +
+                "(" +
+                user.zipcode +
+                ")"}
+            </Td>
+          </Tr>
+        ))}
+      </Table>
+    </ListContainer>
+  );
 };
 
 export default UserList;
+
+const ListContainer = styled.div`
+  text-align: center;
+`;
+
+const Table = styled.table`
+  border: 1px solid #04b486;
+  border-radius: 10px;
+  margin: 0 auto;
+  border-collapse: collapse;
+`;
+
+const Th = styled.th`
+  border: 1px solid #04b486;
+  padding: 10px;
+`;
+
+const Tr = styled.tr``;
+
+const Td = styled.td`
+  border: 1px solid #04b486;
+  padding: 10px;
+`;
